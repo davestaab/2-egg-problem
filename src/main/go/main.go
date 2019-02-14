5665ad83b490aec99643beb47fde640ff56be89f
Mark Brown <mark.brown@cadre5.com>
2019-02-13 19:58:00 -0500

    Changed imports to . imports.  This reduced the number of characters used.

--- src/main/go/main.go
+++ src/main/go/main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"os"
-	"strconv"
+	. "fmt"
+	. "math"
+	. "os"
+	. "strconv"
 )
 
 func main() {
-	n, _ := strconv.Atoi(os.Args[2])
-	v, _ := strconv.Atoi(os.Args[1])
-	g := int(math.Ceil(math.Sqrt(float64(v))))
-	i := 0
+	n, _ := Atoi(Args[2])
+	v, _ := Atoi(Args[1])
+	g, i := int(Ceil(Sqrt(float64(v)))), 0
 
 	f := func(p int) {
 		for i < n {
 			i += p
-			fmt.Printf("%d\n%t\n", i, i >= n)
+			Printf("%d\n%t\n", i, i >= n)
 		}
 	}
 	f(g)
 	i -= g
 	f(1)
-	fmt.Printf("Floor %d", i)
+	Printf("Floor %d", i)
 }
